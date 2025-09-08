e25d8ffb93823e378c18367f16f62235efc8f435
Scott Rosenberg <scott@terasky.com>
2025-09-08 13:29:37 +0300

    redesign ingestor for 1.42

--- plugins/kubernetes-ingestor/src/index.ts
+++ plugins/kubernetes-ingestor/src/index.ts
@@ -1,1 +1,3 @@
-export { catalogModuleKubernetes as default } from './module';
+export { catalogModuleKubernetesIngestor as default } from './module';
+export { KubernetesEntityProvider, XRDTemplateEntityProvider } from './providers';
+export type { KubernetesResourceFetcher, KubernetesResourceFetcherOptions } from './types';
